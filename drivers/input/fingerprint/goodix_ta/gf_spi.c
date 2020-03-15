a0f2fd5cbb77a68b4e27988767d80c09e3a471a8
LenHackintosh <1403727621@qq.com>
2020-03-15 23:24:45 +0800

    Revert "fingerprint: goodix_ta: use qcom drm notifier for event blank calls"
    This reverts commit 90fe3f3a66cf7c2693cc50681260f44a6093f206.

--- drivers/input/fingerprint/goodix_ta/gf_spi.c
+++ drivers/input/fingerprint/goodix_ta/gf_spi.c
@@ -801,7 +801,7 @@ static int goodix_fb_state_chg_callback(struct notifier_block *nb,
 	unsigned int blank;
 	char temp[4] = { 0x0 };
 
-	if (val != MSM_DRM_EVENT_BLANK) {
+	if (val != DRM_EVENT_BLANK) {
 		return 0;
 	}
 
@@ -809,7 +809,7 @@ static int goodix_fb_state_chg_callback(struct notifier_block *nb,
 		 __func__, (int)val);
 	gf_dev = container_of(nb, struct gf_dev, notifier);
 
-	if (evdata && evdata->data && val == MSM_DRM_EVENT_BLANK && gf_dev) {
+	if (evdata && evdata->data && val == DRM_EVENT_BLANK && gf_dev) {
 		blank = *(int *)(evdata->data);
 
 		switch (blank) {
